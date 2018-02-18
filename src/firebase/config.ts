d8d6c3c47c3f92473e9183a1177893d4bdcbb386
Jun Kato <i@junkato.jp>
2018-02-19 00:13:45 +0900

    Start implementing firebase client

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+
+export const config = {
+  apiKey: 'AIzaSyAu2V4z-wqHbvBFHebXxE4hvSeo8ryNV9Q',
+  authDomain: 'radiosync-7a101.firebaseapp.com',
+  databaseURL: 'https://radiosync-7a101.firebaseio.com',
+  projectId: 'radiosync-7a101',
+  storageBucket: '',
+  messagingSenderId: '674063943984'
+};
